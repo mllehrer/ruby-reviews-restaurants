10da4a948bcc7ad3dec4c9fd930564a5fc06fd75
Mia Lehrer <mia.lehrer@gmail.com>
2016-01-14 20:58:58 -0800

    Adds geolocation

--- public/js/application.js
+++ public/js/application.js
@@ -4,9 +4,44 @@ $(document).ready(function() {
 function initialize() {
   var mapCanvas = document.getElementById('map');
   var mapOptions = {
-      center: new google.maps.LatLng(44.5403, -78.5463),
-      zoom: 8,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    }
+    center: new google.maps.LatLng(37.7845, -122.3971),
+    zoom: 8,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  }
   var map = new google.maps.Map(mapCanvas, mapOptions);
-  };
+  var thisLatLng = { lat: 37.7845, lng: -122.3971};
+  var marker = new google.maps.Marker({
+    position: thisLatLng,
+    map: map,
+    title: 'Hello World!'
+  });
+
+var infoWindow = new google.maps.InfoWindow({map: map});
+
+  // Try HTML5 geolocation.
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+
+      infoWindow.setPosition(pos);
+      infoWindow.setContent('Location found.');
+      map.setCenter(pos);
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+                        'Error: The Geolocation service failed.' :
+                        'Error: Your browser doesn\'t support geolocation.');
+};
+
